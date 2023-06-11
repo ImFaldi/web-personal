5af2fb0d3d425c22b83adbff1b5bd8d8be5e8070
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-11 13:39:48 +0700

    Layout Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,14 +13,4 @@ class DashboardController extends Controller
         return view('dashboard.dashboard');
     }
 
-    public function getme()
-    {
-        $token = session()->get('token');
-        $reponse = Http::withToken($token)->get('http://localhost:3000/api/me');
-        $user = json_decode($reponse->body(), true);
-        dd($user);
-        return response()->json([
-            'user' => $user,
-        ]);
-    }
 }
